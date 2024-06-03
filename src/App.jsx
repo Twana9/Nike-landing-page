3063251553d607c3cc042ffbef474c0ac2abb3c0
twana koye <twana.00569020@gmail.com>
2024-06-03 21:56:19 +0300

    version 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <h1
+      className="text-3xl font-bold underline text-center
+    m-9 bg-violet-400 rounded-lg align-center"
+    >
+      Hello world!
+    </h1>
+  );
+}
